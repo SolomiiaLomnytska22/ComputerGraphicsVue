08a915952a82550f517a3b8f0a1e60e512d92233
Solomiia Lomnytska <solomiia.lomnytska.pz.2021@lpnu.ua>
2023-10-15 16:54:35 +0300

    Working on Julia fractal.

--- src/components/JuliaFractal.vue
+++ src/components/JuliaFractal.vue
@@ -21,55 +21,65 @@
         <label for="zoom">Zoom:</label>
         <input type="range" id="zoom" v-model="zoom" min="0" max="7" step="1"/>
       </div>
-      <v-btn class="btn" variant="flat" background-color = "white" color="#D73246" @click="draw">Build Fractal</v-btn>
+      <v-btn class="btn" variant="flat" background-color = "white" color="#D73246" @click="redrawFractal">Build Fractal</v-btn>
     </div>
-    <canvas ref="fractalCanvas" id="fractal"></canvas>
+    <div ref="canvasContainer" id="fractal" :width="600" :height="740"></div>
    
   </div>
   
 </template>
 
 <script>
+import p5 from 'p5';
+
 export default {
   data() {
     return {
+      angle: 0,
       maxIterations: 100,
       zoom: 4,
       ca: -0.1,
       cb: -0.8,
       color: "#ff0000",
+      p5Instance: null,
     };
   },
   mounted() {
-    this.setup();
-    this.draw();
+    this.createCanvas();
   },
   methods: {
-    setup() {
-      const canvas = this.$refs.fractalCanvas;
-      canvas.width = 600;
-      canvas.height = 740;
+    createCanvas() {
+      this.p5Instance = new p5(this.fractalSketch, this.$refs.canvasContainer);
     },
-    draw() {
-      console.log(this.zoom);
-      const canvas = this.$refs.fractalCanvas;
-      const ctx = canvas.getContext("2d");
+    redrawFractal() {
+      this.p5Instance.remove();
+      this.createCanvas();
+    },
+    fractalSketch(p) {
+      p.setup = () => {
+        p.createCanvas(600, 740);
+        p.colorMode(p.HSB);
+        this.drawFractal(p);
+      };
+    },
+    drawFractal(p) {
+      let w = this.zoom;
+      let h = (w * p.height) / p.width;
+      let xmin = -w / 2;
+      let ymin = -h / 2;
 
-      const w = this.zoom;
-      const h = (w * canvas.height) / canvas.width;
-      const xmin = -w / 2;
-      const ymin = -h / 2;
+      p.loadPixels();
 
-      const xmax = xmin + w;
-      const ymax = ymin + h;
+      let xmax = xmin + w;
+      let ymax = ymin + h;
 
-      const dx = (xmax - xmin) / canvas.width;
-      const dy = (ymax - ymin) / canvas.height;
+      let dx = (xmax - xmin) / p.width;
+      let dy = (ymax - ymin) / p.height;
 
       let y = ymin;
-      for (let j = 0; j < canvas.height; j++) {
+      for (let j = 0; j < p.height; j++) {
         let x = xmin;
-        for (let i = 0; i < canvas.width; i++) {
+        for (let i = 0; i < p.width; i++) {
           let a = x;
           let b = y;
           let n = 0;
@@ -84,19 +94,27 @@ export default {
             b = twoab + this.cb;
             n++;
           }
-          if (n === this.maxIterations) {
-            const hue = ((n / this.maxIterations) * 360) % 360;
-            const c = `hsl(${hue}, 100%, ${(n / this.maxIterations) ** 0.25 * 100}%)`;
-            ctx.fillStyle = c;
-            ctx.fillRect(i, j, 1, 1);
+
+          let pix = 4 * (i + j * p.width);
+
+          if (n == this.maxIterations) {
+            p.pixels[pix] = 255;
+            p.pixels[pix + 1] = 255;
+            p.pixels[pix + 2] = 193;
+            p.pixels[pix + 3] = 255;
           } else {
-            ctx.fillStyle = this.color; 
-            ctx.fillRect(i, j, 1, 1);
+            let hu = ((n / this.maxIterations) * 360) % 360;
+            let c = p.color(hu, 100, Math.pow(n / this.maxIterations, 0.25) * 100);
+            p.pixels[pix] = p.blue(c);
+            p.pixels[pix + 1] = p.red(c);
+            p.pixels[pix + 2] = p.red(c);
+            p.pixels[pix + 3] = 255;
           }
           x += dx;
         }
         y += dy;
       }
+      p.updatePixels();
     },
   },
 };
