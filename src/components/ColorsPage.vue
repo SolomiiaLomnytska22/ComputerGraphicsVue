638e9f9b30750c78b6673b1c4daa053ef9182b82
Maksym Pelyno <fteq.qwwerty.com@gmail.com>
2023-10-29 14:02:11 +0200

    Fix colors convert problem

--- src/components/ColorsPage.vue
+++ src/components/ColorsPage.vue
@@ -1,130 +1,135 @@
 <template>
-    
-    <div class = "main-content">
+  <div class="main-content">
+    <div class="page-heading">
+      <h1 class="caption">COLOR SCHEMES</h1>
+      <a href="https://www.geeksforgeeks.org/difference-between-rgb-cmyk-hsv-and-yiq-color-models/" target="_blank">
+        <div class="learn-more-btn"></div>
+      </a>
+    </div>
 
-     <div class="page-heading">
-         <h1 class="caption">COLOR SCHEMES</h1>
-         <a href="https://www.geeksforgeeks.org/difference-between-rgb-cmyk-hsv-and-yiq-color-models/" target="_blank">
-            <div class="learn-more-btn"></div>
-        </a>
-     </div>
-
-     <div class = "content">
-         <div class="toolbar-btn">
-            <div class="buttons">
-            <ImageUploader  style="margin-right:10px"  @file-selected="handleFileSelected"></ImageUploader>
-            <v-btn class="btn" variant="flat" color="#D73246" @click="saveImage">Save
-                <svg-icon style = "padding-left: 5px; color: #FFFFFF" type="mdi" :path="mdiContentSaveAll"></svg-icon>
+    <div class="content">
+      <div class="toolbar-btn">
+        <div class="buttons">
+          <ImageUploader style="margin-right: 10px" @file-selected="handleFileSelected"></ImageUploader>
+          <v-btn class="btn" variant="flat" color="#D73246" @click="saveImage"
+            >Save
+            <svg-icon style="padding-left: 5px; color: #ffffff" type="mdi" :path="mdiContentSaveAll"></svg-icon>
+          </v-btn>
+          <span class="filename">{{ uploadedFileName }}</span>
+        </div>
+        <img src="../assets/Icon.svg" alt="Info" @click="navigateTo('')" />
+      </div>
+      <div class="work-area">
+        <div class="picture-container">
+          <canvas ref="imageCanvas" alt="Your Image" @mousemove="getPixelColor"></canvas>
+          <div class="toolbar">
+            <v-btn
+              class="btn"
+              variant="text"
+              color="white"
+              @mousedown="buttonPressed = true"
+              @mouseup="buttonPressed = false"
+            >
+              <svg-icon style="color: #ffffff" type="mdi" :path="mdiEyeOff"></svg-icon>
             </v-btn>
-            <span class="filename" >{{ uploadedFileName }}</span>
-            </div>
-             <img src="../assets/Icon.svg" alt="Info" @click="navigateTo('')"  >
+            <span class="info"
+              ><h3 style="display: inline">HSV:</h3>
+              {{ formattedHSV }}
+            </span>
+            <span class="info"
+              ><h3 style="display: inline">CMYK:</h3>
+              {{ formattedCMYK }}</span
+            >
+          </div>
+          <canvas v-show="!buttonPressed" ref="newImageCanvas" alt="Your Image" @mousemove="getPixelColor"></canvas>
         </div>
-         <div class = "work-area">
-            <div class = "picture-container"> 
-                <canvas ref= "imageCanvas" alt="Your Image"  @mousemove="getPixelColor"></canvas>
-                    <div class="toolbar">
-                        <v-btn class="btn" variant="text" color="white"  @mousedown="buttonPressed = true" @mouseup="buttonPressed = false" >
-                            <svg-icon style = "color: #ffffff" type="mdi" :path="mdiEyeOff"></svg-icon>
-                        </v-btn>
-                        <span class="info"><h3 style="display:inline">HSV:</h3> {{ formattedHSV  }} </span>
-                        <span class="info"><h3 style="display:inline">CMYK:</h3> {{ formattedCMYK }}</span>
-                    </div>
-                    <canvas v-show="!buttonPressed" ref= "newImageCanvas" alt="Your Image"  @mousemove="getPixelColor"></canvas>
-            </div >
-            <div class = "input-form" style="height: auto;">
-            <p class="title">Change the brightness and saturation of the blue color:</p>
-            <div class="input-field" >
-                <label for="brightness">Brightness:</label>
-                <input type="range" id="brightness" min="-1" max="1" step="0.01"  v-model="brightnessValue"/>
-            </div>
-        <div class="input-field">
+        <div class="input-form" style="height: auto">
+          <p class="title">Change the brightness and saturation of the blue color:</p>
+          <div class="input-field">
+            <label for="brightness">Brightness:</label>
+            <input type="range" id="brightness" min="-1" max="1" step="0.01" v-model="brightnessValue" />
+          </div>
+          <div class="input-field">
             <label for="saturation">Saturation:</label>
-            <input type="range" id="saturation"  min="-1" max="1" step="0.01" v-model="saturationValue" />
+            <input type="range" id="saturation" min="-1" max="1" step="0.01" v-model="saturationValue" />
+          </div>
+          <v-btn class="btn" variant="flat" color="#D73246" @click="adjustSaturationAndBrightness">Apply</v-btn>
         </div>
-        <v-btn class="btn" variant="flat" color="#D73246" @click="adjustSaturationAndBrightness" >Apply</v-btn>
-        </div>
+      </div>
     </div>
 
-     </div>
-
-
-     <div style ="display: flex; justify-content: flex-start; width: 100%">
-        <v-btn class="btn" variant="flat" color="#D73246" @click="navigateTo('main-page')">&lt;</v-btn>
-     </div>
-
+    <div style="display: flex; justify-content: flex-start; width: 100%">
+      <v-btn class="btn" variant="flat" color="#D73246" @click="navigateTo('main-page')">&lt;</v-btn>
     </div>
- </template>
- 
- <script>
- 
- import SvgIcon from '@jamescoyle/vue-icon';
-import { mdiUpload, mdiContentSaveAll, mdiEyeOff} from '@mdi/js';
-import ImageUploader from './ImageUploader.vue';
-import { hsvToRgb, rgbToHsv, rgbToCmyk/*, cmykToRgb*/ } from './ConvertionFunctions'
-import { saveAs } from 'file-saver';
- export default{
-    data() {
+  </div>
+</template>
+
+<script>
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiUpload, mdiContentSaveAll, mdiEyeOff } from "@mdi/js";
+import ImageUploader from "./ImageUploader.vue";
+import { hsvToRgb, rgbToHsv, rgbToCmyk /*, cmykToRgb*/ } from "./ConvertionFunctions";
+import { saveAs } from "file-saver";
+export default {
+  data() {
     return {
-       mdiUpload, 
-       mdiContentSaveAll,
-       mdiEyeOff,
-       selectedImage:"/PlaceHolder.png",
-       uploadedFileName:"",
-       saturationValue: 0,
-       brightnessValue: 0,
-       pixelColor: { rgb: 'N/A', cmyk: 'N/A', hsv: 'N/A' },
-       buttonPressed: false,
-    }
+      mdiUpload,
+      mdiContentSaveAll,
+      mdiEyeOff,
+      selectedImage: "/PlaceHolder.png",
+      uploadedFileName: "",
+      saturationValue: 0,
+      brightnessValue: 0,
+      pixelColor: { rgb: "N/A", cmyk: "N/A", hsv: "N/A" },
+      buttonPressed: false,
+    };
   },
-    computed: {
+  computed: {
     formattedHSV() {
-        if(this.pixelColor.hsv==="N/A")
-        {
-            return(this.pixelColor.hsv);
-        }
-      const roundedValues = this.pixelColor.hsv.map((value) => (Math.round(value * 1000) / 1000));
-      return roundedValues.join(', ');
+      if (this.pixelColor.hsv === "N/A") {
+        return this.pixelColor.hsv;
+      }
+      const roundedValues = this.pixelColor.hsv.map((value) => Math.round(value * 1000) / 1000);
+      return roundedValues.join(", ");
     },
     formattedCMYK() {
-        if(this.pixelColor.cmyk==="N/A")
-        {
-            return(this.pixelColor.cmyk);
-        }
-      const roundedValues = this.pixelColor.cmyk.map((value) => (Math.round(value * 1000) / 1000));
-      return roundedValues.join(', ');
+      if (this.pixelColor.cmyk === "N/A") {
+        return this.pixelColor.cmyk;
+      }
+      const roundedValues = this.pixelColor.cmyk.map((value) => Math.round(value * 1000) / 1000);
+      return roundedValues.join(", ");
     },
   },
-     methods: {
+  methods: {
+    navigateTo(route) {
+      this.$router.push({ name: route });
+    },
+    saveImage() {
+      const canvas = this.$refs.imageCanvas;
+      canvas.toBlob((blob) => {
+        saveAs(blob, this.uploadedFileName);
+      });
+    },
+    handleFileSelected(data) {
+      this.selectedImage = data.selectedImage;
+      this.uploadedFileName = data.uploadedFileName;
 
-       navigateTo(route) {
-         this.$router.push({ name: route });
-       },
-       saveImage(){
-        const canvas = this.$refs.imageCanvas;
-        canvas.toBlob((blob)=> {saveAs(blob, this.uploadedFileName);});
-       },
-       handleFileSelected(data)
-       {
-        this.selectedImage = data.selectedImage;
-        this.uploadedFileName = data.uploadedFileName;
-
-        this.drawImage(this.$refs.imageCanvas);
-        this.drawImage(this.$refs.newImageCanvas);
-
-       },
-       drawImage(canvas){
-        const context = canvas.getContext('2d');
-        const image = new Image();
-        image.src = this.selectedImage;
-        image.onload = () => {
+      this.drawImage(this.$refs.imageCanvas);
+      this.drawImage(this.$refs.newImageCanvas);
+    },
+    drawImage(canvas) {
+      const context = canvas.getContext("2d");
+      const image = new Image();
+      image.src = this.selectedImage;
+      image.onload = () => {
         canvas.width = image.width;
         canvas.height = image.height;
         context.drawImage(image, 0, 0);
-       }
-    }, getPixelColor(event) {
+      };
+    },
+    getPixelColor(event) {
       const canvas = this.$refs.imageCanvas;
-      const ctx = canvas.getContext('2d');
+      const ctx = canvas.getContext("2d");
       const x = event.offsetX;
       const y = event.offsetY;
 
@@ -136,160 +141,161 @@ import { saveAs } from 'file-saver';
       this.pixelColor = {
         rgb: `rgb(${r}, ${g}, ${b})`,
         cmyk: rgbToCmyk(r, g, b),
-        hsv: rgbToHsv(r, g, b)
+        hsv: rgbToHsv(r, g, b),
       };
     },
     adjustSaturationAndBrightness() {
-        let pixels, pos;
-        const originalCanvas = this.$refs.imageCanvas;
-        console.log(this.saturationValue);
-        
-        pixels = originalCanvas.getContext("2d").getImageData(0, 0, originalCanvas.width, originalCanvas.height).data;
-        
-        for (let i = 0; i < originalCanvas.height; ++i) {
-            for (let j = 0; j < originalCanvas.width; ++j) {
-            pos = i * originalCanvas.width + j;
-            pos *= 4;
-            var pixel = {
-                r: pixels[pos],
-                g: pixels[pos + 1],
-                b: pixels[pos + 2],
-            };
-            var hsv = rgbToHsv(pixel.r, pixel.g, pixel.b);
-            if(hsv[0]*360<200||hsv[0]*360>260){
-                hsv.s += this.saturationValue;
-                hsv.v += this.brightnessValue;
-                var rgb = hsvToRgb(hsv.h, hsv.s, hsv.v);
-                pixels[pos] = rgb.r;
-                pixels[pos + 1] = rgb.g;
-                pixels[pos + 2] = rgb.b;
-            }
-            }
-        }
-        this.$refs.newImageCanvas.getContext("2d").putImageData(new ImageData(pixels, originalCanvas.width), 0, 0);
-    },
+      let pixels;
+      const originalCanvas = this.$refs.imageCanvas;
+      // console.log(this.saturationValue);
 
-},
- components:{
+      pixels = originalCanvas.getContext("2d").getImageData(0, 0, originalCanvas.width, originalCanvas.height).data;
+
+      for (let i = 0; i < originalCanvas.height; ++i) {
+        for (let j = 0; j < originalCanvas.width; ++j) {
+          let pos = i * originalCanvas.width + j;
+          pos *= 4;
+          var pixel = {
+            r: pixels[pos],
+            g: pixels[pos + 1],
+            b: pixels[pos + 2],
+          };
+          var hsv = rgbToHsv(pixel.r, pixel.g, pixel.b);
+
+          if (hsv[0] * 360 > 200 && hsv[0] * 360 < 260) {
+            hsv[1] = 0;
+            hsv[2] = 1;
+            var rgb = hsvToRgb(hsv[0], hsv[1], hsv[2]);
+            console.log(rgb);
+            pixels[pos] = rgb[0];
+            pixels[pos + 1] = rgb[1];
+            pixels[pos + 2] = rgb[2];
+          }
+        }
+      }
+      this.$refs.newImageCanvas.getContext("2d").putImageData(new ImageData(pixels, originalCanvas.width), 0, 0);
+    },
+  },
+  components: {
     SvgIcon,
-    ImageUploader
- },
- mounted(){
+    ImageUploader,
+  },
+  mounted() {
     this.drawImage(this.$refs.imageCanvas);
     this.drawImage(this.$refs.newImageCanvas);
- },
- 
- }
- </script>
- 
- <style scoped>
-    @import './fractals-styles.css';
+  },
+};
+</script>
 
-    .buttons{
-        display: flex; flex-direction: row; justify-content: space-between; align-items: center; width: 250px;
-    }
-    .toolbar-btn{
-        display: flex; flex-direction: row; justify-content: space-between; 
-        padding:0px;
-    }
-    canvas:hover{
-     cursor: pointer;
-    }
- .learn-more-btn{
-     cursor: pointer;
-     height: 65px;
-     width: 415px;
-     background-size: 100% 100%;
-     background-image: url('../assets/AboutBtn.svg');
- }
- 
- .learn-more-btn:hover {
-     background-image: url('../assets/AboutBtnPressed.svg');
- }
- .info{
-    padding: 0px 5px; margin: 0;
- }
- .caption
-   {
-     color: white;
-     font-family: 'Roboto', sans-serif;
-     font-size: 40px;
-     margin: 0;
-     font-weight: bold;
-   }
-   .title{
-    color: white; font-size: 18px; padding-bottom: 30px; font-weight: 600;  
-   }
- .page-heading
- {
-     width: 100%;
-     display: flex;
-     flex-direction: row;
-     justify-content: space-between;
- }
- .main-content
- {
-     background: linear-gradient(0deg, rgba(215, 50, 70, 0.40) 0%, rgba(217, 217, 217, 0.00) 100%);
-     display: flex;
-     flex-direction: column;
-     align-items: center;
-     padding: 100px 165px;
- }
- .content
- {
-     width: 100%;
-     background-color: #04101A;
-     padding: 80px;
-     margin:75px 165px;
-   
- }
+<style scoped>
+@import "./fractals-styles.css";
 
- .work-area{
-    display: flex;
-    flex-direction: row;
-    margin-top: 50px;
-    justify-content: space-between;
- }
+.buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 250px;
+}
+.toolbar-btn {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0px;
+}
+canvas:hover {
+  cursor: pointer;
+}
+.learn-more-btn {
+  cursor: pointer;
+  height: 65px;
+  width: 415px;
+  background-size: 100% 100%;
+  background-image: url("../assets/AboutBtn.svg");
+}
 
- .picture-container{
-    height: 375px;
-    width: 575px;
-    background-color: #1F3244;
-    border-radius: 5px;
- }
- 
+.learn-more-btn:hover {
+  background-image: url("../assets/AboutBtnPressed.svg");
+}
+.info {
+  padding: 0px 5px;
+  margin: 0;
+}
+.caption {
+  color: white;
+  font-family: "Roboto", sans-serif;
+  font-size: 40px;
+  margin: 0;
+  font-weight: bold;
+}
+.title {
+  color: white;
+  font-size: 18px;
+  padding-bottom: 30px;
+  font-weight: 600;
+}
+.page-heading {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.main-content {
+  background: linear-gradient(0deg, rgba(215, 50, 70, 0.4) 0%, rgba(217, 217, 217, 0) 100%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 100px 165px;
+}
+.content {
+  width: 100%;
+  background-color: #04101a;
+  padding: 80px;
+  margin: 75px 165px;
+}
 
- .picture-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: stretch;
-    position: relative;
-    overflow: hidden;
+.work-area {
+  display: flex;
+  flex-direction: row;
+  margin-top: 50px;
+  justify-content: space-between;
+}
+
+.picture-container {
+  height: 375px;
+  width: 575px;
+  background-color: #1f3244;
+  border-radius: 5px;
+}
+
+.picture-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: stretch;
+  position: relative;
+  overflow: hidden;
 }
 
 .picture-container canvas {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 
 .toolbar {
-    background: rgba(4, 16, 26, 0.5); 
-    color: white;
-    padding: 10px;
-    z-index: 1;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+  background: rgba(4, 16, 26, 0.5);
+  color: white;
+  padding: 10px;
+  z-index: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 
-.filename
-{
-    color: #FFFFFF; margin-left:10px
+.filename {
+  color: #ffffff;
+  margin-left: 10px;
 }
-
-
- </style>
+</style>
--- src/components/ConvertionFunctions.js
+++ src/components/ConvertionFunctions.js
@@ -1,101 +1,97 @@
 function cmykToRgb(c, m, y, k) {
-    var r = 255 * (1 - c) * (1 - k);
-    var g = 255 * (1 - m) * (1 - k);
-    var b = 255 * (1 - y) * (1 - k);
-  
-    return [Math.round(r), Math.round(g), Math.round(b)];
+  var r = 255 * (1 - c) * (1 - k);
+  var g = 255 * (1 - m) * (1 - k);
+  var b = 255 * (1 - y) * (1 - k);
+
+  return [Math.round(r), Math.round(g), Math.round(b)];
+}
+
+function rgbToCmyk(r, g, b) {
+  r = r / 255;
+  g = g / 255;
+  b = b / 255;
+
+  var k = 1 - Math.max(r, g, b);
+  var c = (1 - r - k) / (1 - k);
+  var m = (1 - g - k) / (1 - k);
+  var y = (1 - b - k) / (1 - k);
+
+  return [c, m, y, k];
+}
+
+function hsvToRgb(hue, saturation, value) {
+  if (hue < 0 || hue > 1) {
+    hue = hue - Math.floor(hue);
   }
-  
-  function rgbToCmyk(r, g, b) {
-    r = r / 255;
-    g = g / 255;
-    b = b / 255;
-  
-    var k = 1 - Math.max(r, g, b);
-    var c = (1 - r - k) / (1 - k);
-    var m = (1 - g - k) / (1 - k);
-    var y = (1 - b - k) / (1 - k);
-  
-    return [c, m, y, k];
+  if (saturation < 0) {
+    saturation = 0;
   }
-  
-  function hsvToRgb(hue, saturation, value) {
-    if (hue < 0 || hue > 1) {
-      hue = hue - Math.floor(hue);
-    }
-    if (saturation < 0) {
-      saturation = 0;
-    }
-    if (saturation > 1) {
-      saturation = 1;
-    }
-    if (value < 0) {
-      value = 0;
-    }
-    if (value > 1) {
-      value = 1;
-    }
-    let chroma = value * saturation;
-    let hue1 = hue * 6;
-    let x = chroma * (1 - Math.abs((hue1 % 2) - 1));
-    let r1 = 0,
-      g1 = 0,
-      b1 = 0;
-  
-    if (hue1 >= 0 && hue1 <= 1) {
-      [r1, g1, b1] = [chroma, x, 0];
-    } else if (hue1 <= 2) {
-      [r1, g1, b1] = [x, chroma, 0];
-    } else if (hue1 <= 3) {
-      [r1, g1, b1] = [0, chroma, x];
-    } else if (hue1 <= 4) {
-      [r1, g1, b1] = [0, x, chroma];
-    } else if (hue1 <= 5) {
-      [r1, g1, b1] = [x, 0, chroma];
-    } else if (hue1 <= 6) {
-      [r1, g1, b1] = [chroma, 0, x];
-    }
-  
-    let m = value - chroma;
-    let [r, g, b] = [r1 + m, g1 + m, b1 + m];
-  
-    return [r, g, b];
+  if (saturation > 1) {
+    saturation = 1;
   }
-  
-  function rgbToHsv(r, g, b) {
-    r /= 255;
-    g /= 255;
-    b /= 255;
-  
-    let max = Math.max(r, g, b),
-      min = Math.min(r, g, b);
-    let h = 0,
-      s = 0,
-      v = max;
-  
-    let d = max - min;
-    s = max === 0 ? 0 : d / max;
-  
-    if (max === min) {
-      h = 0; // achromatic
-    } else {
-      switch (max) {
-        case r:
-          h = (g - b) / d + (g < b ? 6 : 0);
-          break;
-        case g:
-          h = (b - r) / d + 2;
-          break;
-        case b:
-          h = (r - g) / d + 4;
-          break;
-        default:
-          break;
-      }
-      h /= 6;
-    }
-  
-    return [h, s, v];
+  if (value < 0) {
+    value = 0;
   }
-  
-  module.exports = { cmykToRgb, rgbToCmyk, hsvToRgb, rgbToHsv };
+  if (value > 1) {
+    value = 1;
+  }
+  let chroma = value * saturation;
+  let hue1 = hue * 6;
+  let x = chroma * (1 - Math.abs((hue1 % 2) - 1));
+  let r1, g1, b1;
+  if (hue1 >= 0 && hue1 <= 1) {
+    [r1, g1, b1] = [chroma, x, 0];
+  } else if (hue1 <= 2) {
+    [r1, g1, b1] = [x, chroma, 0];
+  } else if (hue1 <= 3) {
+    [r1, g1, b1] = [0, chroma, x];
+  } else if (hue1 <= 4) {
+    [r1, g1, b1] = [0, x, chroma];
+  } else if (hue1 <= 5) {
+    [r1, g1, b1] = [x, 0, chroma];
+  } else if (hue1 <= 6) {
+    [r1, g1, b1] = [chroma, 0, x];
+  }
+  let m = value - chroma;
+  let [r, g, b] = [r1 + m, g1 + m, b1 + m];
+
+  return [255 * r, 255 * g, 255 * b];
+}
+
+function rgbToHsv(r, g, b) {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  let max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  let h = 0,
+    s = 0,
+    v = max;
+
+  let d = max - min;
+  s = max === 0 ? 0 : d / max;
+
+  if (max === min) {
+    h = 0; // achromatic
+  } else {
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+      default:
+        break;
+    }
+    h /= 6;
+  }
+
+  return [h, s, v];
+}
+
+module.exports = { cmykToRgb, rgbToCmyk, hsvToRgb, rgbToHsv };
