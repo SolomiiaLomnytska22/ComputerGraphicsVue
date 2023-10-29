137d51697be811791ea939545677ab9745d5b1b7
Solomiia Lomnytska <solomiia.lomnytska.pz.2021@lpnu.ua>
2023-10-29 13:28:14 +0200

    Added coordinates display, save function.

--- src/components/ColorsPage.vue
+++ src/components/ColorsPage.vue
@@ -4,14 +4,16 @@
 
      <div class="page-heading">
          <h1 class="caption">COLOR SCHEMES</h1>
-         <a href="https://www.geeksforgeeks.org/difference-between-rgb-cmyk-hsv-and-yiq-color-models/" target="_blank"><div class="learn-more-btn"></div></a>
+         <a href="https://www.geeksforgeeks.org/difference-between-rgb-cmyk-hsv-and-yiq-color-models/" target="_blank">
+            <div class="learn-more-btn"></div>
+        </a>
      </div>
 
      <div class = "content">
          <div class="toolbar-btn">
             <div class="buttons">
             <ImageUploader  style="margin-right:10px"  @file-selected="handleFileSelected"></ImageUploader>
-            <v-btn class="btn" variant="flat" color="#D73246">Save
+            <v-btn class="btn" variant="flat" color="#D73246" @click="saveImage">Save
                 <svg-icon style = "padding-left: 5px; color: #FFFFFF" type="mdi" :path="mdiContentSaveAll"></svg-icon>
             </v-btn>
             <span class="filename" >{{ uploadedFileName }}</span>
@@ -20,25 +22,27 @@
         </div>
          <div class = "work-area">
             <div class = "picture-container"> 
-                <canvas ref= "imageCanvas" alt="Your Image"></canvas>
+                <canvas ref= "imageCanvas" alt="Your Image"  @mousemove="getPixelColor"></canvas>
                     <div class="toolbar">
-                        <v-btn class="btn" variant="text" color="white">
+                        <v-btn class="btn" variant="text" color="white"  @mousedown="buttonPressed = true" @mouseup="buttonPressed = false" >
                             <svg-icon style = "color: #ffffff" type="mdi" :path="mdiEyeOff"></svg-icon>
                         </v-btn>
-                        <span class="info">HSV: X.X.X </span><span class="info">CMYK: X.X.X.X</span>
+                        <span class="info"><h3 style="display:inline">HSV:</h3> {{ formattedHSV  }} </span>
+                        <span class="info"><h3 style="display:inline">CMYK:</h3> {{ formattedCMYK }}</span>
                     </div>
+                    <canvas v-show="!buttonPressed" ref= "newImageCanvas" alt="Your Image"  @mousemove="getPixelColor"></canvas>
             </div >
             <div class = "input-form" style="height: auto;">
             <p class="title">Change the brightness and saturation of the blue color:</p>
             <div class="input-field" >
                 <label for="brightness">Brightness:</label>
-                <input type="range" id="brightness" min="1" max="255" step="1" />
+                <input type="range" id="brightness" min="-1" max="1" step="0.01"  v-model="brightnessValue"/>
             </div>
         <div class="input-field">
             <label for="saturation">Saturation:</label>
-            <input type="range" id="saturation"  min="1" max="255" step="1" />
+            <input type="range" id="saturation"  min="-1" max="1" step="0.01" v-model="saturationValue" />
         </div>
-        <v-btn class="btn" variant="flat" color="#D73246" >Apply</v-btn>
+        <v-btn class="btn" variant="flat" color="#D73246" @click="adjustSaturationAndBrightness" >Apply</v-btn>
         </div>
     </div>
 
@@ -57,17 +61,59 @@
  import SvgIcon from '@jamescoyle/vue-icon';
 import { mdiUpload, mdiContentSaveAll, mdiEyeOff} from '@mdi/js';
 import ImageUploader from './ImageUploader.vue';
+import { hsvToRgb, rgbToHsv, rgbToCmyk/*, cmykToRgb*/ } from './ConvertionFunctions'
+import { saveAs } from 'file-saver';
  export default{
+    data() {
+    return {
+       mdiUpload, 
+       mdiContentSaveAll,
+       mdiEyeOff,
+       selectedImage:"/PlaceHolder.png",
+       uploadedFileName:"",
+       saturationValue: 0,
+       brightnessValue: 0,
+       pixelColor: { rgb: 'N/A', cmyk: 'N/A', hsv: 'N/A' },
+       buttonPressed: false,
+    }
+  },
+    computed: {
+    formattedHSV() {
+        if(this.pixelColor.hsv==="N/A")
+        {
+            return(this.pixelColor.hsv);
+        }
+      const roundedValues = this.pixelColor.hsv.map((value) => (Math.round(value * 1000) / 1000));
+      return roundedValues.join(', ');
+    },
+    formattedCMYK() {
+        if(this.pixelColor.cmyk==="N/A")
+        {
+            return(this.pixelColor.cmyk);
+        }
+      const roundedValues = this.pixelColor.cmyk.map((value) => (Math.round(value * 1000) / 1000));
+      return roundedValues.join(', ');
+    },
+  },
      methods: {
+
        navigateTo(route) {
          this.$router.push({ name: route });
+       },
+       saveImage(){
+        const canvas = this.$refs.imageCanvas;
+        canvas.toBlob((blob)=> {saveAs(blob, this.uploadedFileName);});
        },
        handleFileSelected(data)
        {
         this.selectedImage = data.selectedImage;
         this.uploadedFileName = data.uploadedFileName;
 
-        const canvas = this.$refs.imageCanvas;
+        this.drawImage(this.$refs.imageCanvas);
+        this.drawImage(this.$refs.newImageCanvas);
+
+       },
+       drawImage(canvas){
         const context = canvas.getContext('2d');
         const image = new Image();
         image.src = this.selectedImage;
@@ -76,34 +122,63 @@ import ImageUploader from './ImageUploader.vue';
         canvas.height = image.height;
         context.drawImage(image, 0, 0);
        }
-     },
-       defaultDraw(){
-        const canvas = this.$refs.imageCanvas;
-        const context = canvas.getContext('2d');
-        const image = new Image();
-        image.src = this.selectedImage;
-        image.onload = () => {
-        canvas.width = image.width;
-        canvas.height = image.height;
-        context.drawImage(image, 0, 0);
-       }
-    }},
+    }, getPixelColor(event) {
+      const canvas = this.$refs.imageCanvas;
+      const ctx = canvas.getContext('2d');
+      const x = event.offsetX;
+      const y = event.offsetY;
+
+      const pixelData = ctx.getImageData(x, y, 1, 1).data;
+      const r = pixelData[0];
+      const g = pixelData[1];
+      const b = pixelData[2];
+
+      this.pixelColor = {
+        rgb: `rgb(${r}, ${g}, ${b})`,
+        cmyk: rgbToCmyk(r, g, b),
+        hsv: rgbToHsv(r, g, b)
+      };
+    },
+    adjustSaturationAndBrightness() {
+        let pixels, pos;
+        const originalCanvas = this.$refs.imageCanvas;
+        console.log(this.saturationValue);
+        
+        pixels = originalCanvas.getContext("2d").getImageData(0, 0, originalCanvas.width, originalCanvas.height).data;
+        
+        for (let i = 0; i < originalCanvas.height; ++i) {
+            for (let j = 0; j < originalCanvas.width; ++j) {
+            pos = i * originalCanvas.width + j;
+            pos *= 4;
+            var pixel = {
+                r: pixels[pos],
+                g: pixels[pos + 1],
+                b: pixels[pos + 2],
+            };
+            var hsv = rgbToHsv(pixel.r, pixel.g, pixel.b);
+            if(hsv[0]*360<200||hsv[0]*360>260){
+                hsv.s += this.saturationValue;
+                hsv.v += this.brightnessValue;
+                var rgb = hsvToRgb(hsv.h, hsv.s, hsv.v);
+                pixels[pos] = rgb.r;
+                pixels[pos + 1] = rgb.g;
+                pixels[pos + 2] = rgb.b;
+            }
+            }
+        }
+        this.$refs.newImageCanvas.getContext("2d").putImageData(new ImageData(pixels, originalCanvas.width), 0, 0);
+    },
+
+},
  components:{
     SvgIcon,
     ImageUploader
  },
  mounted(){
-    this.defaultDraw();
+    this.drawImage(this.$refs.imageCanvas);
+    this.drawImage(this.$refs.newImageCanvas);
  },
- data() {
-    return {
-       mdiUpload, 
-       mdiContentSaveAll,
-       mdiEyeOff,
-       selectedImage:"/PlaceHolder.png",
-       uploadedFileName:""
-    }
-  }
+ 
  }
  </script>
  
